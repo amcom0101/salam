7b3547c417fb0f493f6a7ed6545196d569dcbef6
amcom0101 <amirr.massoumi@gmail.com>
2023-12-06 16:07:22 +0000

    added file 'calculator.c'

--- calculator.c
+++ calculator.c
@@ -3,3 +3,15 @@ int calculate(int a, int b) {
     // Some calculation
     return a * b;
 }
+#include <stdio.h>
+
+int calculate(int a, int b) {
+	// A different calculation
+	return a + b;
+}
+#include <stdio.h>
+
+int calculate(int a, int b) {
+        // A different calculation
+        return a + b;
+}
